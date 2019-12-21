3b6bfa3967eb368bf4b91b5f2c25410bede21341
TheNexusAvenger <zpcook@googlemail.com>
2019-12-21 01:36:35 -0500

    Added HttpRequest tests

---
+++ NexusHttpServerTests/Http/Request/HttpRequestTests.cs
@@ -0,0 +1,38 @@
+/*
+ * TheNexusAvenger
+ * 
+ * Test the NexusGit.Http.Request.URL class.
+ */
+
+using Nexus.Http.Server.Http.Request;
+using NUnit.Framework;
+
+namespace Nexus.Http.Server.Test.Http.Request
+{
+    [TestFixture]
+    public class HttpRequestTests
+    {
+        /*
+         * Tests that the constructor works without failing.
+         */
+        [Test]
+        public void ConstructorTest()
+        {
+            // Create the component under testing with a string url and assert it was created correctly.
+            var CuT1 = new HttpRequest("POST","/test/page?param=test","https://my.domain","Test body");
+            Assert.AreEqual(CuT1.GetRequestType(),"POST");
+            Assert.AreEqual(CuT1.GetURL().GetBaseURL(),"test/page");
+            Assert.AreEqual(CuT1.GetURL().GetParameter("param"),"test");
+            Assert.AreEqual(CuT1.GetHost(),"https://my.domain");
+            Assert.AreEqual(CuT1.GetBody(),"Test body");
+            
+            // Create the component under testing with a url type and assert it was created correctly.
+            var CuT2 = new HttpRequest("POST",URL.FromString("/test/page?param=test"),"https://my.domain","Test body");
+            Assert.AreEqual(CuT2.GetRequestType(),"POST");
+            Assert.AreEqual(CuT2.GetURL().GetBaseURL(),"test/page");
+            Assert.AreEqual(CuT2.GetURL().GetParameter("param"),"test");
+            Assert.AreEqual(CuT2.GetHost(),"https://my.domain");
+            Assert.AreEqual(CuT2.GetBody(),"Test body");
+        }
+    }
+}
